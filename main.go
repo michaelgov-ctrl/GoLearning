b5f53c50dbcf5d556956d533538fc1c7590d59ed
michaelgov-ctrl <81777732+michaelgov-ctrl@users.noreply.github.com>
2023-11-02 14:50:59 -0400

    Update main.go

--- main.go
+++ main.go
@@ -4,53 +4,91 @@ package main
 
 import (
 	"encoding/csv"
+	"fmt"
 	"log"
 	"math/rand"
 	"os"
 	"strconv"
 )
 
-type QuestionFormat struct {
+type questionFormat struct {
 	NumOne int
 	NumTwo int
 }
 
-func (qF QuestionFormat) EvaluateAddition() int {
+func (qF questionFormat) EvaluateAddition() int {
 	return qF.NumOne + qF.NumTwo
 }
 
-func MakeRangeOfRandomQuestionFormat(min, max int) []QuestionFormat {
-	numRange := make([]QuestionFormat, max-min+1)
+func makeRangeOfRandomQuestionFormat(min, max int) []questionFormat {
+	numRange := make([]questionFormat, max-min+1)
 	for i := range numRange {
-		numRange[i] = QuestionFormat{NumOne: rand.Intn(100), NumTwo: rand.Intn(100)}
+		numRange[i] = questionFormat{NumOne: rand.Intn(100), NumTwo: rand.Intn(100)}
 	}
 	return numRange
 }
 
-func MakeQuiz(questionsMin, questionsMax int) {
-	RandomQuestionsRange := MakeRangeOfRandomQuestionFormat(1, 100)
+func makeQuiz(questionsMin, questionsMax int) {
+	randomQuestionsRange := makeRangeOfRandomQuestionFormat(1, 100)
+	filePath := "problems.csv"
 
-	csvFile, err := os.Create("problems.csv")
-	defer csvFile.Close()
+	csvFile, err := os.Create(filePath)
 	if err != nil {
 		log.Fatalf("failed creating file: %s\n", err)
 	}
+	defer csvFile.Close()
 
 	csvWriter := csv.NewWriter(csvFile)
 
 	var data [][]string
 
 	data = append(data, []string{"equation", "answer"})
-	for _, questions := range RandomQuestionsRange {
+	for _, questions := range randomQuestionsRange {
 		equation := strconv.Itoa(questions.NumOne) + "+" + strconv.Itoa(questions.NumTwo)
 		row := []string{equation, strconv.Itoa(questions.EvaluateAddition())}
 		data = append(data, row)
 	}
 
-	csvWriter.WriteAll(data)
+	err = csvWriter.WriteAll(data)
+	if err != nil {
+		log.Fatalf("unable to write to file: %s %s\n", filePath, err)
+	}
+}
+
+func getQuiz(filePath string) [][]string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("failed to read input file: %s %s\n", filePath, err)
+	}
+	defer f.Close()
+
+	csvReader := *csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatalf("unable to parse csv as file: %s %s\n", filePath, err)
+	}
+
+	return records
 
 }
 
+func takeQuiz(qs [][]string) {
+	//reader := bufio.NewReader(os.Stdin)
+	correctCount := 0
+	for _, val := range qs {
+		fmt.Print("what is: ", val[0], "= ")
+		var input int
+		fmt.Scan(&input) //reader.ReadString('\n')
+		if input == val[1] {
+			fmt.Println("correct")
+			correctCount++
+		} else {
+			fmt.Println("incorrect")
+		}
+	}
+}
+
 func main() {
-	MakeQuiz(1, 100)
+	makeQuiz(1, 100)
+	takeQuiz(getQuiz("problems.csv"))
 }
