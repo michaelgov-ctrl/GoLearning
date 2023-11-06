aa331ea18607a3183bf49d0e11827bb31368a307
michaelgov-ctrl <81777732+michaelgov-ctrl@users.noreply.github.com>
2023-11-06 17:09:46 -0500

    Update LilHttpRequest.go

--- LilHttpRequest.go
+++ LilHttpRequest.go
@@ -1,22 +1,36 @@
+package main
 
-func main() {
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func getRemoteContent(method string, url string) (int64, error) {
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", "http://www.golang.org", nil)
+	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
-		fmt.Println("error: ", err)
-		return
+		return 0, err
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-
 	req = req.WithContext(ctx)
-
 	resp, err := client.Do(req)
 	cancel()
 	if err != nil {
-		fmt.Println("error: ", err)
-		return
+		return 0, err
 	}
 
-	io.Copy(os.Stdout, resp.Body)
+	stdOut, err := io.Copy(os.Stdout, resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	return stdOut, nil
+}
+
+func main() {
+	getRemoteContent("GET", "https://raw.githubusercontent.com/michaelgov-ctrl/AzureLearning/main/ARMTemplates/DebianVM/azuredeploy.json")
 }
