a4f4fe7dcdc6dbc9ad94ace8ec73157b74256d4b
michaelgov-ctrl <81777732+michaelgov-ctrl@users.noreply.github.com>
2023-11-13 22:47:09 -0500

    Create flags.go

---
+++ commandlindTool/flags.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// number is a natural number.
+type number int
+
+// toNumber is a convenience function for converting p to *number.
+func toNumber(p *int) *number {
+	return (*number)(p)
+}
+
+func (n *number) Set(s string) error {
+	v, err := strconv.ParseInt(s, 0, strconv.IntSize)
+	switch {
+	case err != nil:
+		err = errors.New("parse error")
+	case v <= 0:
+		err = errors.New("should be positive")
+	}
+	*n = number(v)
+	return err
+}
+
+func (n *number) String() string {
+	return strconv.Itoa(int(*n))
+}
+
+type flags struct {
+	url  string
+	n, c int
+}
+
+func validateURL(s string) error {
+	u, err := url.Parse(s)
+	switch {
+	case strings.TrimSpace(s) == "":
+		err = errors.New("required")
+	case err != nil:
+		err = errors.New("parse error")
+	case u.Scheme != "http":
+		err = errors.New("only supported scheme is http")
+	case u.Host == "":
+		err = errors.New("missing host")
+	}
+	return err
+}
+
+// 'method' to validate the Concurrency and Number of requests properties of a flags struct
+func (f *flags) validate() error {
+	if f.c > f.n {
+		fmt.Printf("-c=%d: must be less than or equal to -n=%d\n", f.c, f.n)
+		f.c = f.n
+	}
+	if err := validateURL(f.url); err != nil {
+		return fmt.Errorf("invalid value %q for flag -url: %w", f.url, err)
+	}
+	return nil
+}
+
+// 'method' to parse properties of a flags struct
+func (f *flags) parse() error {
+	flag.StringVar(&f.url, "url", "", "HTTP server `URL` to make requests (required)")
+	flag.Var(toNumber(&f.n), "n", "Number of requests to make")
+	flag.Var(toNumber(&f.c), "c", "Concurrency level")
+	flag.Parse()
+	if err := f.validate(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		flag.Usage()
+		return err
+	}
+	return nil
+}
