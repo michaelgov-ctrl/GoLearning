75da51bda4f420a928c0859e318a330d2c5f4fce
michaelgov-ctrl <81777732+michaelgov-ctrl@users.noreply.github.com>
2024-01-13 00:38:38 -0500

    Update shiftArrayRight.go

--- shiftArrayRight.go
+++ shiftArrayRight.go
@@ -1,4 +1,16 @@
-//https://leetcode.com/problems/create-target-array-in-the-given-order/solutions/4501228/go-0ms-runtime/
+// https://leetcode.com/problems/duplicate-zeros/description/
+// 1 method
+func duplicateZeros(arr []int)  {
+    for i := 0 ; i < len(arr) ; i++ {
+        if arr[i] == 0 {
+            arr = append(arr[:i+1], arr[i:len(arr)-1]...)
+            i++
+        }
+    }
+}
+
+// https://leetcode.com/problems/create-target-array-in-the-given-order/solutions/4501228/go-0ms-runtime/
+// another method
 
 func shiftArrayValuesRight(idx int, arr []int ) []int {
     for i := len(arr)-1 ; i > idx ; i-- {
