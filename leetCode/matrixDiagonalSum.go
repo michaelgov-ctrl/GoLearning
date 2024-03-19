605dc6e64fae691bf512c34ec62d64618bfd43ea
michaelgov-ctrl <81777732+michaelgov-ctrl@users.noreply.github.com>
2024-03-18 23:24:04 -0400

    Create matrixDiagonalSum.go

---
+++ leetCode/matrixDiagonalSum.go
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/matrix-diagonal-sum/
+
+func diagonalSum(mat [][]int) int {
+    if len(mat) == 1 {
+        return mat[0][0]
+    }
+
+    var sum int
+    for i, j, x, y := 0, 0, 0, len(mat)-1; i < len(mat[0]) && y < len(mat); i, j, x, y = i+1, j+1, x+1, y-1 {
+        sum+=mat[i][j]
+        sum+=mat[x][y]
+    }
+
+    l := len(mat)
+    if l%2 != 0 {
+        sum-=mat[(l/2)][(l/2)]
+    }
+
+    return sum
+}
