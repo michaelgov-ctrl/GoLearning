76d36662f19cba390a9c01c28149f0fd2015604f
michaelgov-ctrl <81777732+michaelgov-ctrl@users.noreply.github.com>
2024-03-22 17:16:13 -0400

    Update binarySearchExamples.go

--- binarySearchExamples.go
+++ binarySearchExamples.go
@@ -68,3 +68,24 @@ func nextGreatestLetter(letters []byte, target byte) byte {
 	}
 	return letters[0]
 }
+
+// https://leetcode.com/problems/search-a-2d-matrix/
+func searchMatrix(matrix [][]int, target int) bool {
+    m := len(matrix)
+    n := len(matrix[0])
+    left, right := 0, m*n-1
+
+    for left <= right {
+        mid := left + (right-left)/2
+        mid_val := matrix[mid/n][mid%n]
+
+        if mid_val == target {
+            return true
+        } else if mid_val < target {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return false
+}
