0e0e8531eb43d832cbabe89b500d342988016869
michaelgov-ctrl <81777732+michaelgov-ctrl@users.noreply.github.com>
2024-07-29 16:08:31 -0400

    Update removeOccurencesFromString.go

--- leetCode/removeOccurencesFromString.go
+++ leetCode/removeOccurencesFromString.go
@@ -1,5 +1,5 @@
-// https://leetcode.com/problems/remove-all-occurrences-of-a-substring/description/
 
+// https://leetcode.com/problems/remove-all-occurrences-of-a-substring/description/
 func removeOccurrences(s string, part string) string {
     idx := strings.Index(s, part)
     for idx != -1 {
@@ -10,6 +10,34 @@ func removeOccurrences(s string, part string) string {
         
         s = before+after
         idx = strings.Index(s, part)
+    }
+
+    return s
+}
+
+// or
+func removeOccurrences(s string, part string) string {
+    for strings.Index(s, part) != -1 {
+        before, after, found := strings.Cut(s, part)
+        if !found {
+            break
+        }
+        
+        s = before+after
+    }
+
+    return s
+}
+
+// or
+func removeOccurrences(s string, part string) string {
+    for {
+        before, after, found := strings.Cut(s, part)
+        if !found {
+            break
+        }
+        
+        s = before+after
     }
 
     return s
