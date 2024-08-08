ba66e3db5a3cf860b6ff09778eca9074a1324edf
michaelgov-ctrl <81777732+michaelgov-ctrl@users.noreply.github.com>
2024-08-08 14:22:57 -0400

    Update gcdAndFraction.go

--- leetCode/gcdAndFraction.go
+++ leetCode/gcdAndFraction.go
@@ -2,10 +2,9 @@
 
 func simplifiedFractions(n int) []string {
     var res []string
-    
     for denominator := 1; denominator <= n; denominator++ {
         for numerator := 1; numerator < denominator; numerator++ {
-            if numerator % denominator != 0 && gcd(numerator, denominator) == 1 {
+            if gcd(numerator, denominator) == 1 {
                 n, d := strconv.Itoa(numerator), strconv.Itoa(denominator)
                 res = append(res, n+"/"+d)
             }
