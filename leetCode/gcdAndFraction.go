7d473214a06483100f2392e8e934ab04c9da0853
michaelgov-ctrl <81777732+michaelgov-ctrl@users.noreply.github.com>
2025-08-25 12:45:00 -0400

    Update gcdAndFraction.go

--- leetCode/gcdAndFraction.go
+++ leetCode/gcdAndFraction.go
@@ -20,3 +20,49 @@ func gcd(a, b int) int {
     }
     return gcd(b,a%b)
 }
+
+
+
+
+// https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+
+type Parity int
+
+const (
+    Even Parity = iota
+    Odd
+)
+
+func (p Parity) Value() int {
+    if p == Even {
+        return 2
+    }
+    return 1
+}
+
+func (p Parity) Sum(n int) int {
+    var sum int
+    v := p.Value()
+    for range n {
+        sum += v
+        v += 2
+    }
+    return sum
+}
+
+func gcd(a, b int) int {
+    if b==0 {
+        return a
+    }
+    return gcd(b,a%b)
+}
+
+func gcdOfOddEvenSums(n int) int {
+    return gcd(Even.Sum(n), Odd.Sum(n))
+}
+
+// or just
+// proof: https://leetcode.com/problems/gcd-of-odd-and-even-sums/solutions/7118260/just-simple-math
+func gcdOfOddEvenSums(n int) int {
+    return n
+}
