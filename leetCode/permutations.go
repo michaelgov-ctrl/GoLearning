320c6859c280612e3af60187020afeb6ec6bd60b
michaelgov-ctrl <81777732+michaelgov-ctrl@users.noreply.github.com>
2026-02-04 09:39:00 -0500

    Update permutations.go

--- leetCode/permutations.go
+++ leetCode/permutations.go
@@ -28,3 +28,79 @@ func subsetsRecursive(nums []int) [][]int {
     dfs(0) // Start the recursion from the first element
     return res
 }
+
+
+
+// https://leetcode.com/problems/maximum-sum-of-three-numbers-divisible-by-three/description/
+func maximumSum(nums []int) int {
+    var max int
+    var current []int
+    used := make([]bool, len(nums))
+
+    var dfs func(current []int)
+    dfs = func(current []int) {
+        if len(current) == 3 {
+            temp := sum(current)
+            if temp % 3 == 0 && temp > max {
+                max = temp
+            }
+
+            return
+        }
+
+        for i := 0; i < len(nums); i++ {
+            if !used[i] {
+                used[i] = true
+                current = append(current, nums[i])
+                dfs(current)
+
+                current = current[:len(current)-1]
+                used[i] = false
+            }
+        }
+    }
+
+    dfs(current)
+
+    return max
+}
+
+func sum(nums []int) int {
+    var sum int
+    for _, n := range nums {
+        sum += n
+    }
+
+    return sum
+}
+
+func generateKPermutations(nums []int, k int) [][]int {
+    var results [][]int
+    var current []int
+    used := make([]bool, len(nums))
+
+    var dfs func(current []int)
+    dfs = func(current []int) {
+        if len(current) == k {
+            temp := make([]int, k)
+            copy(temp, current)
+            results = append(results, temp)
+            return
+        }
+
+        for i := 0; i < len(nums); i++ {
+            if !used[i] {
+                used[i] = true
+                current = append(current, nums[i])
+                dfs(current)
+
+                current = current[:len(current)-1]
+                used[i] = false
+            }
+        }
+    }
+
+    dfs(current)
+
+    return results
+}
