b60b0e782e18cc8f10c3a6fc9064cf7c02bc252a
Mike G <81777732+michaelgov-ctrl@users.noreply.github.com>
2026-09-02 14:19:38 -0400

    Update tree.go

--- leetCode/tree.go
+++ leetCode/tree.go
@@ -50,3 +50,37 @@ func preorderTraversal(root *TreeNode) []int {
 
     return res
 }
+
+
+
+
+// https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func postorderTraversal(root *TreeNode) []int {
+    var res []int
+
+    var postorder func(node *TreeNode)
+    postorder = func(node *TreeNode) {
+        if node == nil {
+            return
+        }
+
+        postorder(node.Left)
+        postorder(node.Right)
+
+        res = append(res, node.Val)
+    }
+
+    postorder(root)
+
+    return res
+}
+
