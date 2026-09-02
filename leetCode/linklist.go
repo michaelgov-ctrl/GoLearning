329cef08d7b4da7ab99c5a555cefc5bfb3288fae
Mike G <81777732+michaelgov-ctrl@users.noreply.github.com>
2026-09-02 00:38:45 -0400

    Update linklist.go

--- leetCode/linklist.go
+++ leetCode/linklist.go
@@ -494,3 +494,27 @@ func toArray(head *Node) []int {
     }
     return res
 }
+
+// https://leetcode.com/problems/print-immutable-linked-list-in-reverse/description/
+
+/*   Below is the interface for ImmutableListNode, which is already defined for you.
+ *
+ *   type ImmutableListNode struct {
+ *       
+ *   }
+ *
+ *   func (this *ImmutableListNode) getNext() ImmutableListNode {
+ *		// return the next node.
+ *   }
+ *
+ *   func (this *ImmutableListNode) printValue() {
+ *		// print the value of this node.
+ *   }
+ */
+
+func printLinkedListInReverse(head ImmutableListNode) {
+    for head != nil {
+        defer head.printValue()
+        head = head.getNext()
+    }
+}
