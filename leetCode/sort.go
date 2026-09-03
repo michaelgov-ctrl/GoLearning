b11936470882bcddb5e7cf6bd60f8500cb349a5c
Mike G <81777732+michaelgov-ctrl@users.noreply.github.com>
2026-09-03 11:32:25 -0400

    Update sort.go

--- leetCode/sort.go
+++ leetCode/sort.go
@@ -121,3 +121,69 @@ func minDeletion(s string, k int) int {
     
     return res
 }
+
+
+
+
+// https://leetcode.com/problems/find-x-sum-of-all-k-long-subarrays-i/description/
+func findXSum(nums []int, k int, x int) []int {
+    res := make([]int, len(nums)-k+1)
+    for i := 0; i <= len(nums)-k; i++ {
+        res[i] = xSum(nums[i:i+k], x)
+    }
+    return res
+}
+
+func sum(nums []int) int {
+    var res int
+    for _, n := range nums {
+        res+=n
+    }
+    return res
+}
+
+type Freq struct {
+    Val, Count int
+}
+
+func newFreq(val, count int) Freq {
+    return Freq{
+        Val: val,
+        Count: count,
+    }
+}
+
+func xSum(nums []int, x int) int {
+    freqMap := make(map[int]int)
+    for _, n := range nums {
+        freqMap[n]++
+    }
+
+    freqArr, i := make([]Freq, len(freqMap)), 0
+    for k, v := range freqMap {
+        freqArr[i] = newFreq(k, v)
+        i++
+    }
+
+    if len(freqArr) <= x {
+        var res int
+        for _, f := range freqArr {
+            res += f.Val * f.Count
+        }
+        return res
+    }
+
+    slices.SortFunc(freqArr, func(a, b Freq) int {
+        if n := cmp.Compare(b.Count, a.Count); n != 0 {
+            return n
+        }
+        return cmp.Compare(b.Val, a.Val)
+    })
+
+    var res int
+    for i := 0; i < x; i++ {
+        res += freqArr[i].Val * freqArr[i].Count
+    }
+
+    return res
+}
